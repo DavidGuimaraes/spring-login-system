af7364714587806500dadbd84f3387a2bf362004
David Guimaraes <david.ce@live.com>
2020-04-05 09:03:15 -0400

    04. Moving from Bearer token to JWT.

--- login-demo/src/main/java/co/davidguimaraes/logindemo/config/AuthorizationServerConfig.java
+++ login-demo/src/main/java/co/davidguimaraes/logindemo/config/AuthorizationServerConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableAuthorizationServer
@@ -33,9 +34,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
                 .authenticationManager(authenticationManager);
     }
 
     @Bean
     public TokenStore tokenStore(){ return new InMemoryTokenStore(); }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey("algaworks");
+        return accessTokenConverter;
+    }
 }
