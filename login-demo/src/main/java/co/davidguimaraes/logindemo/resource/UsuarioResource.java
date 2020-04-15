3035b9f966754bd3f486799121a33e3b5f648a21
David Guimaraes <david.ce@live.com>
2020-04-15 14:52:16 -0400

    28. Changing endpoints to return DTOs instead of Entities.

--- login-demo/src/main/java/co/davidguimaraes/logindemo/resource/UsuarioResource.java
+++ login-demo/src/main/java/co/davidguimaraes/logindemo/resource/UsuarioResource.java
@@ -1,6 +1,8 @@
 package co.davidguimaraes.logindemo.resource;
 
 import co.davidguimaraes.logindemo.model.Usuario;
+import co.davidguimaraes.logindemo.resource.dto.UsuarioDto;
+import co.davidguimaraes.logindemo.resource.mapper.UsuarioMapper;
 import co.davidguimaraes.logindemo.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -22,32 +26,42 @@ public class UsuarioResource {
     @Autowired
     private UsuarioService service;
 
+    @Autowired
+    private UsuarioMapper mapper;
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("isAuthenticated() and hasAuthority('ROLE_PESQUISAR_USUARIO') and #oauth2.hasScope('read')")
-    public Page<Usuario> listar(Pageable pageable) {
-        return service.findAll(pageable);
+    public Page<UsuarioDto> listar(Pageable pageable) {
+        pageable = pageable.previousOrFirst();
+        Page<Usuario> usuarios = service.findAll(pageable);
+        return usuarios.map(mapper::entityToDto);
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("isAuthenticated() and hasAuthority('ROLE_PESQUISAR_USUARIO') and #oauth2.hasScope('read')")
-    public ResponseEntity<Usuario> pesquisarPeloId(@PathVariable Long id) {
+    public ResponseEntity<UsuarioDto> pesquisarPeloId(@PathVariable Long id) {
         Optional<Usuario> usuario = service.findById(id);
-        return usuario.isPresent() ? ResponseEntity.ok(usuario.get()) : ResponseEntity.notFound().build();
+        Optional<UsuarioDto> dto = usuario.map(mapper::entityToDto);
+        return usuario.isPresent() ? ResponseEntity.ok(dto.get()) : ResponseEntity.notFound().build();
     }
 
     @GetMapping(params = "email", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("isAuthenticated() and hasAuthority('ROLE_PESQUISAR_USUARIO') and #oauth2.hasScope('read')")
-    public ResponseEntity<Usuario> pesquisarPeloEmail(@RequestParam String email) {
+    public ResponseEntity<UsuarioDto> pesquisarPeloEmail(@RequestParam String email) {
         Optional<Usuario> usuario = service.findByEmail(email);
-        return usuario.isPresent() ? ResponseEntity.ok(usuario.get()) : ResponseEntity.notFound().build();
+        Optional<UsuarioDto> dto = usuario.map(mapper::entityToDto);
+        return usuario.isPresent() ? ResponseEntity.ok(dto.get()) : ResponseEntity.notFound().build();
     }
 
     @PostMapping
     @PreAuthorize("isAuthenticated() and hasAuthority('ROLE_CADASTRAR_USUARIO') and #oauth2.hasScope('write')")
-    public ResponseEntity<Usuario> criar(@Valid @RequestBody Usuario usuario, HttpServletResponse response) {
+    public ResponseEntity<UsuarioDto> criar(@Valid @RequestBody Usuario usuario, HttpServletResponse response) {
         Usuario usuarioSalvo = service.save(usuario);
 //        publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
-        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioSalvo);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuarioSalvo.getIdUsuario()).toUri();
+        return ResponseEntity.created(uri).body(mapper.entityToDto(usuarioSalvo));
+//        return ResponseEntity.status(HttpStatus.CREATED).body(mapper.entityToDto(usuarioSalvo));
     }
 
     @DeleteMapping("/{id}")
